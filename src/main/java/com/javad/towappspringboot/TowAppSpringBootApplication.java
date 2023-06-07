4513da4425de379acbb1fb97dfa90a6d37bc7f07
AbdiJavad <Mirjavadabdi@gmail.com>
2023-06-07 07:39:23 +0330

    main

---
+++ src/main/java/com/javad/towappspringboot/TowAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.javad.towappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TowAppSpringBootApplication.class, args);
+    }
+
+}
